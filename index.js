cf2462c6219cd62bc835a6ac3dd2b23260263f6d
Jerroyd Moore <jerroydmoore@gmail.com>
2018-07-22 16:21:02 -0700

    WIP

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const fs = require('fs-extra');
 const Path = require('path');
 const EOL = require('os').EOL;
 const crypto = require('crypto');
-const inquirer = require('inquirer');
 const program = require('commander');
 
 const ALGO = 'sha256';
 
 var cwd = undefined;
-program.version('1.0.1')
+program.version('1.1.0')
    .usage('[options] <directory>')
-  .option('-r, --review <n>', 'How many collisions to review [all]', parseInt, 0)
+  .option('-o, --output <path>', 'Where to write the file, otherwise, just print to stdout')
   .action(function (dir) { cwd = dir; })
   .parse(process.argv);
 
@@ -19,6 +18,8 @@ if ( !cwd || Number.isNaN(program.review)) {
 }
 
 var hashMap = new Map();
+var fileSize = new Map();
+
 function readDir(cwd, hashMap) {
   return fs.readdir(cwd).then((files) => {
     let pAll = [ ];
@@ -37,6 +38,7 @@ function readDir(cwd, hashMap) {
             //console.log(relPath);
             if ( !hashMap.has(checksum)) {
               hashMap.set(checksum, [ relPath ]);
+              fileSize.set(checksum, stats.size);
             } else {
               let list = hashMap.get(checksum);
               list.push(relPath);
@@ -59,65 +61,43 @@ function readDir(cwd, hashMap) {
   }); // fs.readdir
 }
 
-var toDelete = [];
-readDir(cwd, hashMap).then((howmany) => {
-  console.log("OK. Checked", howmany, "items");
-  let entries = Array.from(hashMap.entries()).map((obj) => obj[1]), // convert Iterator to Array
-      itor = 0;
+readDir(cwd, hashMap).then((fileCount) => {
+  console.log("OK. Checked", fileCount, "items");
 
   // only look at dups
-  entries = entries.filter((x) => x.length > 1);
-  console.log("", entries.length, "collisions detected");
-
-  if (program.review && entries.length > program.review) {
-    entries.length = program.review;
-    console.log("Limiting to", entries.length, "collisions");
-  }
-
-  let questions = entries.map((list) => {
-    list.sort();
-    return {
-      type: "checkbox",
-      name: "" + (itor++),
-      message: "Select which duplicate files to delete.",
-      choices: list,
-      default: list.slice(0, -1) // select everything but the last item
-    };
-  });
-
-  return inquirer.prompt(questions);
-}).then((answers) => {
-  for (let key in answers) {
-    if (answers[key] !== undefined) {
-      toDelete.push(answers[key]);
+  let keys = Array.from(hashMap.keys());
+  for (let checksum of keys) {
+    let files = hashMap.get(checksum);
+    if (files.length < 2) {
+      hashMap.delete(checksum);
+      fileSize.delete(checksum);
     }
   }
-  //toDelete = Array.from(answers.entries()).map(o => o[1]).reduce((arr, curr) => {
-  //toDelete = Array.from(answers.values()).reduce((arr, curr) => {
-  toDelete = toDelete.reduce((arr, curr) => {
 
-    arr.push(...curr);
-    return arr;
-  }, [ ]);
+  let dup_space = 0;
 
-  console.log("", toDelete.length, "files to delete.");
-  toDelete.forEach((x) => console.log("\t", x));
-  return inquirer.prompt({
-    type: "confirm",
-    name: "delete",
-    message: "Are you sure you want to delete " + toDelete.length + " files?",
-    default: true
+  // convert HashMap to an object with a checksum and a list of paths
+  let collisions = Array.from(hashMap.entries()).map((obj) => {
+    let [checksum, paths] = obj;
+    let size = fileSize.get(checksum);
+    dup_space +=  size * (paths.length-1); // substract 1: we'll keep one copy
+    return {checksum, size, paths};
   });
-}).then((confirm) => {
-  if(confirm.delete) {
-    return Promise.all(toDelete.map((file) => fs.remove(file)))
-      .then(() => toDelete.length);
+
+  return {
+    "files_checked": fileCount,
+    "number_of_collisions": hashMap.size,
+    "bytes_occupied_by_dups": dup_space,
+    "data": collisions
+  };
+}).then((results) => {
+  let results_str = JSON.stringify(results, null, 4);
+  if(program.output) {
+    return fs.writeFile(program.output, results_str, 'utf8');
   } else {
-    return 0;
+    console.log(results_str);
   }
-}).then((howmany) => {
-  console.log("", howmany, "files deleted.");
-});
+})
 
 function calculateSha256(filePath) {
   return new Promise((resolve) => {
